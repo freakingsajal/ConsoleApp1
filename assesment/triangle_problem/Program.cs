7c84e03b3c646a2ef9649378ecfa0beb328a76ae
freaking sajal <sajal.official550@gmail.com>
2022-08-09 17:35:30 +0530

    assesment_1

---
+++ assesment/triangle_problem/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace triangle_problem
+{
+    internal class Program
+    {
+        static void checkTriangle(int x, int y, int z)
+        {
+
+            
+            if (x == y && y == z)
+                Console.WriteLine("Equilateral Triangle");
+
+            
+            else if (x == y || y == z || z == x)
+                Console.WriteLine("Isosceles Triangle");
+
+           
+            else
+                Console.WriteLine("Scalene Triangle");
+        }
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Enter the side of traingle");
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            z = Convert.ToInt32(Console.ReadLine());
+            checkTriangle(x, y, z);
+        }
+    }
+}
