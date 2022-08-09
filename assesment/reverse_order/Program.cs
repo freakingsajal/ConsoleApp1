f0e062447c925049eb0ddb0f0011880df92c0f23
freaking sajal <sajal.official550@gmail.com>
2022-08-09 16:52:44 +0530

    assesment_1

---
+++ assesment/reverse_order/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace reverse_order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstLetter, secondLetter1, thirdLetter2;
+
+            Console.Write("Input first letter: ");
+            firstLetter = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Input second letter: ");
+            secondLetter1 = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Input third letter: ");
+            thirdLetter2 = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("{0} {1} {2}", thirdLetter2, secondLetter1, firstLetter);
+        }
+    }
+}
