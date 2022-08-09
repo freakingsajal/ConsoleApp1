d84b3ef706253deedd0a438793e59f942a365c6e
freaking sajal <sajal.official550@gmail.com>
2022-08-09 17:10:34 +0530

    assesment_1

---
+++ assesment/even or odd/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace even_or_odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            bool bothnumbers;
+            Console.Write("Input First number: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input Second number: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+
+            if((n1 % 2 == 0) && (n2 % 2 == 0))
+            {
+                bothnumbers = true;
+            }
+            else
+            {
+                bothnumbers = false;
+            }
+            
+
+            Console.WriteLine(bothnumbers ? "Both numbers are Even" : "Both number are odd");
+        }
+    }
+}
