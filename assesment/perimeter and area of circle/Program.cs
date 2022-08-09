8614b1e30e21f26f0c9507b2ce4b459c6e0b5a13
freaking sajal <sajal.official550@gmail.com>
2022-08-09 16:59:05 +0530

    assesment_1

---
+++ assesment/perimeter and area of circle/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace perimeter_and_area_of_circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius, perimeterOfCircle,area;
+            double PI = 3.14;
+            Console.WriteLine("Input the radius of the circle : ");
+            radius = Convert.ToDouble(Console.ReadLine());
+            perimeterOfCircle = 2 * PI * radius;
+            Console.WriteLine("Perimeter of Circle : " +  perimeterOfCircle);
+            area = PI * radius * radius;
+            Console.WriteLine("Area of a circle : " + area);
+            
+        }
+    }
+}
