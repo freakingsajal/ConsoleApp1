95ed9310c9be23f2c7b8a13598b771332e49b22c
freaking sajal <sajal.official550@gmail.com>
2022-08-10 18:20:12 +0530

    assignment_2

---
+++ assignment-2(day3)/replace lowercase characters by uppercase and vice-versa/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace replace_lowercase_characters_by_uppercase_and_vice_versa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1;
+            char[] arr1;
+            int length, i;
+            length = 0;
+            char ch;
+   
+            Console.Write("Input the string : ");
+            str1 = Console.ReadLine();
+            length = str1.Length;
+            arr1 = str1.ToCharArray(0, length); 
+
+            Console.Write("\nAfter conversion, the string is : ");
+            for (i = 0; i < length; i++)
+            {
+                ch = arr1[i];
+                if (Char.IsLower(ch))
+                    Console.Write(Char.ToUpper(ch));
+                else
+                    Console.Write(Char.ToLower(ch));
+            }
+            Console.Write("\n\n");
+        }
+    }
+}
