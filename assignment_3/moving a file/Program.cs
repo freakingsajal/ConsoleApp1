1939104662e2b0f6ba7007ebdce1944264d6ebaa
freaking sajal <sajal.official550@gmail.com>
2022-08-11 18:14:21 +0530

    assignment_3

---
+++ assignment_3/moving a file/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace moving_a_file
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sfileName = "D:\\backend trainning asp.net\\example5.txt";
+            string tfileName = "D:\\backend trainning asp.net\\example6.txt";
+            if (File.Exists(sfileName))
+            {
+                File.Delete(sfileName);
+            }
+            if (File.Exists(tfileName))
+            {
+                File.Delete(tfileName);
+            }
+            using (StreamWriter fileStr = File.CreateText(sfileName))
+            {
+                fileStr.WriteLine(" Hello and Welcome");
+                fileStr.WriteLine(" It is the first content");
+                
+            }
+            using (StreamReader sr = File.OpenText(sfileName))
+            {
+                string s = "";
+                Console.WriteLine(" Here is the content of the file {0} : ", sfileName);
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine("");
+            }
+            System.IO.File.Move(sfileName, tfileName); 
+            Console.WriteLine(" The file has successfully moved to the same directory.", sfileName, tfileName);
+        }
+    }
+}
