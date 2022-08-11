7a9c2db3530951a8fcfd43a6f973ca63d50103b2
freaking sajal <sajal.official550@gmail.com>
2022-08-11 18:07:47 +0530

    assignment_3

---
+++ assignment_3/Append text/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Append_text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myfile = "D:\\backend trainning asp.net\\example4.txt";
+
+
+            if (!File.Exists(myfile))
+            {
+                
+                using (StreamWriter sw = File.CreateText(myfile))
+                {
+                    sw.WriteLine("Hello");
+                    sw.WriteLine("world");
+                    
+                }
+            }
+
+            
+            using (StreamWriter sw = File.AppendText(myfile))
+            {
+                sw.WriteLine("I");
+                sw.WriteLine("am");
+                sw.WriteLine("sajal");
+            }
+
+            
+            using (StreamReader sr = File.OpenText(myfile))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
