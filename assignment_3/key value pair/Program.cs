de766f11c38f0ac48716057cbc808af34f90c2ca
freaking sajal <sajal.official550@gmail.com>
2022-08-11 18:23:34 +0530

    assignment_3

---
+++ assignment_3/key value pair/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace key_value_pair
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int,int> StudentInfo = new Dictionary<int,int>();
+
+
+            StudentInfo.Add(1, 40);
+            StudentInfo.Add(2, 90);
+            StudentInfo.Add(3, 80);
+            StudentInfo.Add(4, 95);
+
+
+            foreach (KeyValuePair<int,int> item in StudentInfo)
+            {
+                Console.WriteLine("Id : " + item.Key + " Marks : " + item.Value);
+            }
+        }
+    }
+}
