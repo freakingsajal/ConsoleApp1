932c7c113822bf159c718cf133c08efce80ebff7
freaking sajal <sajal.official550@gmail.com>
2022-08-11 18:33:43 +0530

    assignment_3

---
+++ assignment_3/Generic Method/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Generic_Method
+{
+    internal class Program
+    {
+        public class genrics
+        {
+            public void Generic<T, X, Y>(T id, X name, Y result_status)
+            {
+                Console.WriteLine("id: " + id + " name: " + name + " Result_Status :" + result_status);
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            genrics genericObj = new genrics();
+            genericObj.Generic(12, "Rahul", 'Y');
+            genericObj.Generic(14, "Kiran", 'N');
+            genericObj.Generic(23, "sajal", 'Y');
+            genericObj.Generic(40, "Ram", 'Y');
+            genericObj.Generic(31, "shyam", 'N');
+        }
+    }
+}
